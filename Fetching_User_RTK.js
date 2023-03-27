4a6d46ab204e811fdb5ebcd9da75444fa8449d51
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-03-27 18:50:16 +0530

    Create Fetching_User_RTK.js

---
+++ Fetching_User_RTK.js
@@ -0,0 +1,31 @@
+      
+
+############ Fetching the Data and Adding the TAGS for Automatics refetc option ################
+
+export const albumsApi = createApi({
+  reducerPath: "albums",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+
+
+  endpoints(builder) {
+    return {
+//   Fetching the Albums
+      // Querying
+      fetchAlbums: builder.query({
+        // We use Tags for auto refetching for some scenarios
+        providesTags: ["Album"],
+        query: (user) => {
+          return {
+            url: "/albums",
+            params: {
+              //   Getting the function based on the users list user id !
+              userId: user.id,
+            },
+            method: "GET",
+          };
+        },
+      }),
+    }
+  })
